3185356342a43e2b369586e1711764cdfa35358e
Balkishun Maurya <bkishun21072001@gmail.com>
2024-03-19 14:47:32 +0530

    project is ready

---
+++ 01reactRouter/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h1 className='bg-green-600 p-4 text-black text-6xl'>React router dom</h1>
+    </>
+  )
+}
+
+export default App
